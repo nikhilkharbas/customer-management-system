67cc298ae03f8ed92ae9d370578a241f07084c4b
nikhilkharbas <nikhilkharbas1208@gmail.com>
2019-12-24 00:10:00 +0530

    initial state

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InputUserDataFormComponent } from './input-user-data-form/input-user-data-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InputUserDataFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
